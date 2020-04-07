e4a03a216732de391484b93f5571efd1d1a92400
Paulo Negrao <paulo.negrao@ymail.com>
2020-04-07 14:12:22 -0700

    Customize CodeDeploy aspec & scripts
    Customize appspeck.yml and ApplicationStart.sh

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -3,6 +3,6 @@
 
 cd /home/app/doorSensor/
 #####      Creating a service call doorSensor
-sudo forever-service install doorSensor -r app
+sudo forever-service install doorSensor -r iot
 ##### start a service called doorSensor
 sudo service doorSensor start
