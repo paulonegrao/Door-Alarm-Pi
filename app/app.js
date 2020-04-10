11ddee5663e609e0ad90a9a6c075813cb2deb14f
Paulo Negrao <paulo.negrao@ymail.com>
2020-04-10 11:07:16 -0700

    aws services image

--- app/app.js
+++ app/app.js
@@ -5,7 +5,8 @@ var globalSerial; // Pi Serial Number
 
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED = new Gpio(26, 'out'); //use GPIO pin 26 as output
-var pushButton = new Gpio(13, 'in', 'both', {debounceTimeout: 100}); //use GPIO pin 13 as input, and 'both' button presses, and releases should be handled
+var pushButton = new Gpio(13, 'in', 'both', { debounceTimeout: 100 }); //use GPIO pin 13 as input, and 'both' button presses, and releases should be handled
+const pushButtonARN = '/doorSensor/sns_arn'; // AWS Parameter Store for this pushButton ARN
 
 const util = require('util');  // required to execute linux command
 const exec = util.promisify(require('child_process').exec); // execute linux command
@@ -28,17 +29,17 @@ async function fMain() {
     var user = await fnUser();  // get linux user
     console.log(`User: ${user}`);
 
-    AWS.config.update({region: myRegion});
+    AWS.config.update({ region: myRegion });
     docClient = new AWS.DynamoDB.DocumentClient();  // client to use with dynamoDB
-    
+
     // Parameter Store
-    const awsParamStore = require( 'aws-param-store' );
+    const awsParamStore = require('aws-param-store');
     //this command is sync, NOT async
-    let parameter = awsParamStore.getParameterSync( '/doorSensor/sns_arn', {region: myRegion});
+    let parameter = awsParamStore.getParameterSync(`${pushButtonARN}`, { region: myRegion });
     arn_sns = parameter.Value;
     console.log(`SNS topic ARN ${arn_sns}`);
     // Parameter Store end
-    
+
     var ButtonStatus = pushButton.readSync();
     var status;
     if (ButtonStatus == 0) {
@@ -48,8 +49,8 @@ async function fMain() {
       LED.write(0);
       status = "Closed!"
     }
-      writeToDynamoDB(status);
-      // sendMessage(status);
+    writeToDynamoDB(status);
+    // sendMessage(status);
   } catch (error) {
     console.error(error);
   }
@@ -58,14 +59,13 @@ async function fMain() {
 pushButton.watch(async function (err, value) { //Watch for hardware interrupts on pushButton GPIO, specify callback function
   if (err) { //if an error
     console.error('There was an error', err); //output error message to console
-  return;
+    return;
   }
   var status;
   if (value == 1) {
     LED.write(0);
     status = "Closed"
-  } else
-  {
+  } else {
     LED.write(1);
     status = "Opened"
   }
@@ -101,37 +101,37 @@ async function fnUser() {  // return AWS region
 function writeToDynamoDB(status) { // putItem on dynamoDB table
   var d = new Date();
   var seconds = Math.round(d.getTime() / 1000);
-  
+
   var params = {
     Item: {
-     "Serial": globalSerial,
-     "date_time": seconds,
-     "status": status     
-    },     
-    ReturnConsumedCapacity: "TOTAL", 
+      "Serial": globalSerial,
+      "date_time": seconds,
+      "status": status
+    },
+    ReturnConsumedCapacity: "TOTAL",
     TableName: "door_sensor"
-   };
+  };
 
-   docClient.put(params, function(err, data) {
+  docClient.put(params, function (err, data) {
     if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response  
- });
+    else console.log(data);           // successful response  
+  });
 }
 
 function sendMessage(status) {
   var d = new Date();
-  var textmessage = new AWS.SNS({apiVersion: '2010-03-31'});
+  var textmessage = new AWS.SNS({ apiVersion: '2010-03-31' });
   textmessage.publish({
     Message: `Device ${globalSerial}: Door is:${status} on ${d}`,  /* required */
     TopicArn: arn_sns
   },
-    function(err,data) {
+    function (err, data) {
       if (err) {
         console.error(err, err.stack);
         return;
       }
-      console.log(data);    
-  });
+      console.log(data);
+    });
 }
 
 function unexportOnClose() { //function to run when exiting program
